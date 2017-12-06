e9b745778b96e4cae30cb96ced0c6235f12db1ca
Tom Prentice <vigilant.hylian@gmail.com>
2017-12-06 21:04:33 +1100

    Added initial migration and recipe model

--- Server/database/migrations/2017_12_06_092857_Initial.php
+++ Server/database/migrations/2017_12_06_092857_Initial.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Initial extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('recipe', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email', 255)->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipes');
     }
 }
